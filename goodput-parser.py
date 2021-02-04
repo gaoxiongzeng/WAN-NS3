a8b0950bbadb7a3c388e528fae6c0cac39b9f02a
曾高雄 <gaoxiongzeng@qq.com>
2021-02-04 19:47:59 +0800

    Update goodput-parser.py

--- goodput-parser.py
+++ goodput-parser.py
@@ -39,5 +39,4 @@ if __name__ == "__main__":
     #for bSize in [3333]: # for test
     for bSize in [333333, 133333, 66666, 33333, 13333, 6666, 3333, 1333, 666, 333, 133]: # in packets (50MB=33.3kMTU)
        file = str(protocol) + "-buf" + str(bSize) + "-static-log"
-       #parse(file)
-  parse("log") # for test
+       parse(file)
